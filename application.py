7d5b6371586c9eb8f98e4279061840e011d7084e
regularfry <alex@blackkettle.org>
2022-10-25 17:28:51 +0100

    Reworked the thank you page

--- application.py
+++ application.py
@@ -379,8 +379,16 @@ def send():
 @application.route('/thanks')
 def thanks():
     return template(page_template, body="""
-    <h1>Thanks</h1>
-    <a href="/">back</a>
+    <h1>Thank you!</h1>
+    
+    <p>
+        Thank you for your input.  We appreciate the effort you've gone to in making us aware of your challenge.
+    </p>
+
+    <p>
+        If you indicated that you wanted to be involved, we will get in touch about our next steps.
+        Please don't hesitate to send us more challenges when you find them.
+    </p>
     """)
 
 def main():
