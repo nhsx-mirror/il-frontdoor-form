8bbee931f517a427712c2b78a15b9a18f8816350
regularfry <alex@blackkettle.org>
2022-10-26 11:56:35 +0100

    Fix slightly broken markup

--- application.py
+++ application.py
@@ -77,8 +77,8 @@ def html_radios(name, legend, options):
 def html_checkbox_item(name, value, label):
     return f"""
       <div class="nhsuk-checkboxes__item">
-        <input class="nhsuk-checkboxes__input" id="{name}" name="{name}" type="checkbox" value="{value}">
-        <label class="nhsuk-label nhsuk-checkboxes__label" for="{name}">
+        <input class="nhsuk-checkboxes__input" id="{name}-{value}" name="{name}" type="checkbox" value="{value}">
+        <label class="nhsuk-label nhsuk-checkboxes__label" for="{name}-{value}">
           {label}
         </label>
       </div>
@@ -162,6 +162,8 @@ page_template = """
     <meta name="msapplication-square150x150logo" content="/static/assets/favicons/mediumtile-150x150.png">
     <meta name="msapplication-wide310x150logo" content="/static/assets/favicons/widetile-310x150.png">
     <meta name="msapplication-square310x310logo" content="/static/assets/favicons/largetile-310x310.png">
+
+    <title>Innovation Lab Front Door</title>
   </head>
   <body>
     <script>document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');</script>
@@ -169,12 +171,14 @@ page_template = """
 
     <header class="nhsuk-header" role="banner">
       <div class="nhsuk-width-container nhsuk-header__container">
-        <div class="nhsuk-header__logo nhsuk-header__logo--only"><a class="nhsuk-header__link nhsuk-header__link--service " href="/" aria-label="NHS homepage">
+        <div class="nhsuk-header__logo nhsuk-header__logo--only">
+          <a class="nhsuk-header__link nhsuk-header__link--service " href="/" aria-label="NHS homepage">
 
-          <svg class="nhsuk-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 16" height="40" width="100">
-            <path class="nhsuk-logo__background" fill="#005eb8" d="M0 0h40v16H0z"></path>
-            <path class="nhsuk-logo__text" fill="#fff" d="M3.9 1.5h4.4l2.6 9h.1l1.8-9h3.3l-2.8 13H9l-2.7-9h-.1l-1.8 9H1.1M17.3 1.5h3.6l-1 4.9h4L25 1.5h3.5l-2.7 13h-3.5l1.1-5.6h-4.1l-1.2 5.6h-3.4M37.7 4.4c-.7-.3-1.6-.6-2.9-.6-1.4 0-2.5.2-2.5 1.3 0 1.8 5.1 1.2 5.1 5.1 0 3.6-3.3 4.5-6.4 4.5-1.3 0-2.9-.3-4-.7l.8-2.7c.7.4 2.1.7 3.2.7s2.8-.2 2.8-1.5c0-2.1-5.1-1.3-5.1-5 0-3.4 2.9-4.4 5.8-4.4 1.6 0 3.1.2 4 .6"></path>
-          </svg>
+            <svg class="nhsuk-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 16" height="40" width="100">
+              <path class="nhsuk-logo__background" fill="#005eb8" d="M0 0h40v16H0z"></path>
+              <path class="nhsuk-logo__text" fill="#fff" d="M3.9 1.5h4.4l2.6 9h.1l1.8-9h3.3l-2.8 13H9l-2.7-9h-.1l-1.8 9H1.1M17.3 1.5h3.6l-1 4.9h4L25 1.5h3.5l-2.7 13h-3.5l1.1-5.6h-4.1l-1.2 5.6h-3.4M37.7 4.4c-.7-.3-1.6-.6-2.9-.6-1.4 0-2.5.2-2.5 1.3 0 1.8 5.1 1.2 5.1 5.1 0 3.6-3.3 4.5-6.4 4.5-1.3 0-2.9-.3-4-.7l.8-2.7c.7.4 2.1.7 3.2.7s2.8-.2 2.8-1.5c0-2.1-5.1-1.3-5.1-5 0-3.4 2.9-4.4 5.8-4.4 1.6 0 3.1.2 4 .6"></path>
+            </svg>
+          </a>
 
           <span class="nhsuk-header__service-name">
             Innovation Lab Front Door: bring us your ideas!
@@ -232,12 +236,10 @@ form_template=f"""
   </p>
 
   <p>
-    Please note that we don't take submissions relating to existing solutions or products.
+    We don't take submissions relating to existing solutions or products.
     Please see our
-    <a href="https://www.nhsx.nhs.uk/nhsx-innovation-lab/send-us-your-challenges/">
-      guidance
-    </a>
-    on what challenges are/aren't suitable.
+    <a href="https://www.nhsx.nhs.uk/nhsx-innovation-lab/send-us-your-challenges/">guidance</a>
+    on what challenges are suitable.
   </p>
 
   <p>
